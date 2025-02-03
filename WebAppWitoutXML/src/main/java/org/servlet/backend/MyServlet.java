f7c81f518fc51d72336090cd0eb8ecb8eacdbf69
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2025-02-03 22:15:06 +0530

    Add files via upload

---
+++ WebAppWitoutXML/src/main/java/org/servlet/backend/MyServlet.java
@@ -0,0 +1,17 @@
+package org.servlet.backend;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+@WebServlet("/aaa")
+public class MyServlet extends HttpServlet{
+@Override
+protected void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+	System.out.println("My service method()");
+}
+}
