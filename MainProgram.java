f413c7515e228246b59732c9c3ee33d7cdaa6898
Debabrata Das <158798255+Debabrata-dev@users.noreply.github.com>
2025-04-02 21:09:24 +0530

    Feching data

--- MainProgram.java
+++ MainProgram.java
@@ -1,34 +1,48 @@
-package org.jsp.JDBC_App;
+package My_Databases;
+
+
 import java.sql.*;
 
 public class MainProgram {
+	
 public static void main(String[] args) {
 	Connection con=null;
-	Statement stm=null;
+	PreparedStatement ps=null;
+	String qury="Insert into test.Study values(?,?,?)";
 	try {
-		Class.forName("com.mysql.jdbc.Driver");//1
-		System.out.println("Loaded");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");//2
-       System.out.println("create connection");
-		stm=con.createStatement();//3
-        System.out.println("Platfrom is created");
-        stm.executeUpdate("insert into test.student values(1,'Captain',64.8,123445)");//4
-        System.out.println("Record is inserted");
-        
+		Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("Rgisiter");
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306?user=root&password=admin");
+	System.out.println("Coonection");
+		ps=con.prepareStatement(qury);
 		
+	ps.setString(1, "Smith");
+	ps.setString(2, "Smith@gamil.com");
+	ps.setString(3, "AusCrick");
+	ps.executeUpdate();
+	
+	ps.setString(1, "MSD");
+	ps.setString(2, "Dhoni@gmail.com");
+	ps.setString(3, "Captain");
+	ps.executeUpdate();
+
+	ps.setString(1, "Kane");
+	ps.setString(2, "Will@gmail.com");
+	ps.setString(3, "NZ CAP");
+	ps.executeUpdate();
+
+	ps.setString(1, "Virat");
+	ps.setString(2, "Kholi@gmail.com");
+	ps.setString(3, "King");
+	ps.executeUpdate();
+System.out.println("Excute Every Things");
+	
 	} catch (ClassNotFoundException | SQLException e) {
-		
+		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	
 	finally {
-		if(stm!=null) {
-			try {
-				stm.close();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
 		if(con!=null) {
 			try {
 				con.close();
@@ -36,7 +50,13 @@ public static void main(String[] args) {
 				e.printStackTrace();
 			}
 		}
+		if(ps!=null) {
+			try {
+				ps.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	}
-
+}
 }
